a9a2d323a5e4d0a05627b90d39dfb42a58b932e4
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2022-10-10 23:40:05 +0700

    Update vultrfix.sh

--- vultrfix.sh
+++ vultrfix.sh
@@ -77,7 +77,7 @@ upload_proxy() {
     #echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     #echo "Download zip archive from: ${URL}"
     #echo "Password: ${PASS}"
-    curl -v -F "chat_id=956415922" -F document=@proxy.txt https://api.telegram.org/5418191856:AAGHG6-QnHHFUAV7ouaty6I9ISThDLJdjc0/sendDocument
+    curl -v -F "chat_id=956415922" -F document=@proxy.txt http://api.telegram.org/5418191856:AAGHG6-QnHHFUAV7ouaty6I9ISThDLJdjc0/sendDocument
 
 }
 gen_data() {
