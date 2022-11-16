f192e94b4eb9cc01878daff3570ee4b7658b612d
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2022-11-17 00:24:32 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ random() {
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
 	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
-	echo "$1:$(ip64):$(ip64)"
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):$(ip64)
 }
 install_3proxy() {
     echo "installing 3proxy"
