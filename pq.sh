52ef92fe17d032e851cac8640d6a252e39aef1eb
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2022-11-17 21:32:36 +0700

    Update pq.sh

--- pq.sh
+++ pq.sh
@@ -11,7 +11,7 @@ gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
 	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):$2"
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):$(ip64):$2"
 }
 install_3proxy() {
     echo "installing 3proxy"
@@ -86,7 +86,7 @@ upload_proxy() {
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6 $LAST_IP6)"
+        echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -113,7 +113,7 @@ mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-2 -d':')
-LAST_IP6=$(curl -6 -s icanhazip.com | cut -f4 -d':')
+#LAST_IP6=$(curl -6 -s icanhazip.com | cut -f4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
