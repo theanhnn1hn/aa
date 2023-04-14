5d59b4b44d3fd532dfb54ee2dbfe395f9f514372
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2023-04-14 22:40:33 +0700

    Update reloadproxy_7.sh

--- reloadproxy_7.sh
+++ reloadproxy_7.sh
@@ -13,7 +13,29 @@ gen64() {
     }
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 1000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth strong
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
 
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "yag/anhbiencong/$IP4/$port/$(gen64 $IP6)"
