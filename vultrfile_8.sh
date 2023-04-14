0a3590839ec6ed12813c869799393045e2d93fd7
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2023-04-14 21:19:56 +0700

    Update vultrfile_8.sh

--- vultrfile_8.sh
+++ vultrfile_8.sh
@@ -134,7 +134,7 @@ gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
 systemctl start NetworkManager.service
-ifup enp1s0
+nmcli connection up ${INTERFACE}
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 65535
