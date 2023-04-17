f581037a6c01d6293e6c30fd83f1365bb0ee4b4d
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2023-04-17 23:06:54 +0700

    Update reloadproxy_8.sh

--- reloadproxy_8.sh
+++ reloadproxy_8.sh
@@ -1,26 +1,34 @@
-#!/bin/bash
-
+#!/bin/sh
+WORKDIR="/home/proxy-installer"
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-main_interface=$(ip route get 8.8.8.8 | awk '{printf $5}')
+main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+num_ipv6=$1
 
 gen64() {
-    ipv6_addr="$1"
-    for i in {1..4}; do
-        ipv6_addr="$ipv6_addr:${array[RANDOM % 16]}${array[RANDOM % 16]}${array[RANDOM % 16]}${array[RANDOM % 16]}"
-    done
-    echo "$ipv6_addr"
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-
 # Remove all existing IPv6 addresses from boot_ifconfig.sh
-sed -i '/inet6/d' /home/proxy-installer/boot_ifconfig.sh
+sed -i '/inet6/d' $WORKDIR/boot_ifconfig.sh
 
 # Remove all existing IPv6 addresses from the main network interface
 ip -6 addr flush dev "$main_interface"
 
-# Add new IPv6 addresses to boot_ifconfig.sh
-for i in $(seq 1 "$1"); do
-  ipv6_addr=$(gen64 "$(curl -6 -s icanhazip.com | cut -f1-4 -d':')")
-  echo "ifconfig $main_interface inet6 add $ipv6_addr/64" >> /home/proxy-installer/boot_ifconfig.sh
-  echo "Added new IPv6 address: $ipv6_addr"
-done
+# Generate new IPv6 addresses
+gen_data() {
+    seq 1 $num_ipv6 | while read count; do
+        echo "$(gen64 $(curl -6 -s icanhazip.com | cut -f1-4 -d':'))"
+    done
+}
+gen_data > $WORKDIR/data.txt
+
+# Add the new IPv6 addresses to the interface
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $1 "/64"}' ${WORKDATA})
+EOF
+}
+gen_ifconfig > $WORKDIR/boot_ifconfig.sh
