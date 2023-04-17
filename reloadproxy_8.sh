4167100424fd968ec72cfaa1048ed15cc9fbcc2e
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2023-04-17 21:30:49 +0700

    Update reloadproxy_8.sh

--- reloadproxy_8.sh
+++ reloadproxy_8.sh
@@ -1,101 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+# Function to generate a random string
+random_string() {
+  tr </dev/urandom -dc A-Za-z0-9 | head -c5
+  echo
 }
 
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+# Array of hexadecimal characters for generating IPv6 addresses
+hex_chars=(0 1 2 3 4 5 6 7 8 9 a b c d e f)
 
-gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+# Get the name of the main network interface
+main_interface=$(ip route get 8.8.8.8 | awk '{printf $5}')
+
+# Function to remove all existing IPv6 addresses from the main network interface
+remove_existing_ipv6_addresses() {
+  existing_ipv6_addresses=$(ip -6 addr show dev "$main_interface" | grep 'inet6' | awk '{print $2}')
+  for addr in $existing_ipv6_addresses; do
+    ip -6 addr del "${addr%/*}" dev "$main_interface"
+  done
 }
 
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "yag/anhbiencong/$IP4/$port/$(gen64 $IP6)"
-    done
+# Function to add new IPv6 addresses to the main network interface
+add_new_ipv6_addresses() {
+  # Generate new IPv6 addresses for the proxies
+  for i in $(seq 1 "$1"); do
+    ipv6_addr=$(printf "%04x:%04x:%04x:%04x" "${hex_chars[RANDOM % 16]}" "${hex_chars[RANDOM % 16]}" "${hex_chars[RANDOM % 16]}" "${hex_chars[RANDOM % 16]}")
+    ip -6 addr add "$ipv6_addr/64" dev "$main_interface"
+  done
 }
 
-gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
+# Remove all existing IPv6 addresses
+remove_existing_ipv6_addresses
 
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 2000
-nserver 1.1.1.1
-nserver 8.8.4.4
-nserver 2001:4860:4860::8888
-nserver 2001:4860:4860::8844
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-stacksize 6291456 
-flush
-auth strong
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
-EOF
-}
-
-gen_proxy_file_for_user() {
-    cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
-EOF
-}
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <number_of_proxies>"
-    exit 1
-fi
-
-num_proxies=$1
-
-echo "working folder = /home/proxy-installer"
-WORKDIR="/home/proxy-installer"
-WORKDATA="${WORKDIR}/data.txt"
-
-IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-
-echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
-
-FIRST_PORT=23000
-LAST_PORT=$(($FIRST_PORT + $num_proxies - 1))
-
-gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x $WORKDIR/boot_*.sh /etc/rc.local
-
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-# Apply the new iptables rules and ifconfig settings
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-
-# Restart 3proxy service to apply new configuration
-systemctl restart 3proxy
-
-# Generate proxy.txt file for the user
-gen_proxy_file_for_user
+# Add new IPv6 addresses for the proxies
+add_new_ipv6_addresses "$1"
