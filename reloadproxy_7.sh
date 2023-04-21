4b0b3637fc9d2dfa8789816062b862e3a52fadae
theanhnn1hn <43785936+theanhnn1hn@users.noreply.github.com>
2023-04-21 11:38:01 +0700

    Update reloadproxy_7.sh

--- reloadproxy_7.sh
+++ reloadproxy_7.sh
@@ -1,14 +1,68 @@
 #!/bin/sh
-WORKDIR="/home/proxy-installer"
+random() {
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
+}
+
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 main_interface=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 1000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth strong
+
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
+
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "yag/anhbiencong/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
@@ -21,61 +75,19 @@ num_proxies=$1
 FIRST_PORT=23000
 LAST_PORT=$(($FIRST_PORT + $num_proxies - 1))
 
-# Generate new IPv6 addresses
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        #echo "$(gen64 $(curl -6 -s icanhazip.com | cut -f1-4 -d':'))"
-        echo "yag/anhbiencong/$IP4/$port/$(gen64 $IP6)"
-    done
-}
-gen_data > $WORKDIR/data.txt
-
-# Add the new IPv6 addresses to the interface
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig '$main_interface' inet6 add " $5 "/64"}' ${WORKDIR}/data.txt)
-EOF
-}
-gen_iptables() {
-    cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDIR}/data.txt)
-EOF
-}
-gen_ifconfig > $WORKDIR/boot_ifconfig.sh
-gen_iptables > $WORKDIR/boot_iptables.sh
-systemctl restart NetworkManager.service
-
-# Generate 3proxy configuration with new IPv6 addresses
-gen_3proxy() {
-    cat <<EOF
-daemon
-maxconn 1000
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-flush
-auth strong
-
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDIR}/data.txt)
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDIR}/data.txt)
-EOF
-}
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-if ! grep -q "bash ${WORKDIR}/boot_iptables.sh" /etc/rc.local; then
-    cat >>/etc/rc.local <<EOF
+cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
-service 3proxy start
 EOF
-fi
 
 bash /etc/rc.local
+
 systemctl restart 3proxy.service
